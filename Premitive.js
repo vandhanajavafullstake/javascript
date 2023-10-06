2a7a30e1690f7ede478ae82477aa1de088f16b0e
vandhanajavafullstake <vandhana12699@gmail.com>
2023-10-06 10:34:16 +0530

    first commit

---
+++ Premitive.js
@@ -0,0 +1,23 @@
+// There are two types of datatype //
+// Primitive datatype = holding single value//
+var price = 499;
+var rating=4.5;
+var brand = 'peterengland'
+var avail ='y'
+var discount=false
+var elocation;
+console.log(price);
+console.log(rating);
+console.log(brand);
+console.log(avail);
+console.log(discount);
+console.log(elocation);
+
+console.log(typeof price);
+console.log(typeof rating);
+console.log(typeof brand);
+console.log(typeof avail);
+console.log(typeof discount);
+console.log(typeof elocation);
+
+
---
+++ non.js
@@ -0,0 +1,10 @@
+// non premitive datatype= multiple values//
+var con=[35,38,40,42,45]
+document.write(typeof size)
+console.log (size)
+/*var size=35,38,40,42,45
+console.log (typeof size)
+console.log (size)*/
+var =100
+var a=10
+console.log(a)
